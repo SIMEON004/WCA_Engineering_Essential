e6dc3318f77f859ea550e4dde5fce680a18987a5
SIMEON004 <simeonplkm@gmail.com>
2026-01-20 17:41:24 +0000

    Build issue resolved

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import WCA from './wca'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <WCA />
